c94c43ea59dd6baeb1748bc269bf889b01543fe8
unknown <mikewhite628@gmail.com>
2023-03-20 23:36:56 -0500

    small updates for deployment

--- src/pages/Plan.js
+++ src/pages/Plan.js
@@ -394,13 +394,16 @@ export const getServerSideProps = withPageAuthRequired({
     const user = session.user;
     const sub = user.sub;
 
-    const fetchDBUser = await fetch("http://localhost:3000/api/getuser", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ sub }),
-    })
+    const fetchDBUser = await fetch(
+      "https://www.aifitnesstrainer.io/api/getuser",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ sub }),
+      }
+    )
       .then((res) => res.json())
       .catch((err) => console.log(err));
 
